1bbcce5a9fdb212dff0322d2b76536e5631c8f69
InJunKangW <149602262+InJunKangW@users.noreply.github.com>
2024-06-14 11:13:43 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/bookstoreinjun/BookstoreInjunApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookstoreinjun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreInjunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreInjunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/bookstoreinjun/BookstoreInjunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookstoreinjun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreInjunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
